3470864da2d93e843ee034e2eecf5e20e51495da
doroteya-agayna <doroteya.agayna@telerik.com>
2014-07-30 11:20:07 +0300

    Remove the files from the repo.

--- Sample/ContextApi.VB/ContextApi.Tests/MessageHelper.vb
+++
@@ -1,8 +0,0 @@
-﻿Public NotInheritable Class MessageHelper
-    Private Sub New()
-    End Sub
-    Public Shared Function NoRecordsInDatabase(ByVal typeOfEntity As Type) As String
-        Dim message As String = String.Format("No records of type {0} in the database!", typeOfEntity.Name)
-        Return message
-    End Function
-End Class
